0ea2ab992babb4fd0837472c711f5163683ecb70
WSY <niaoniaov@126.com>
2019-01-23 09:34:52 +0800

    项目初版

---
+++ app/src/main/java/com/italkbb/apklist/AppsAdapter.java
@@ -0,0 +1,140 @@
+package com.italkbb.apklist;
+
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by WangSiYe on 2018/12/10.
+ * 测试更改
+ */
+public class AppsAdapter<T> extends RecyclerView.Adapter<AppsAdapter.ViewHolder> {
+
+
+    public interface OnItemClickListener {
+        void onItemClick(View v);
+    }
+
+    public interface OnItemSelectedListener {
+        void onItemSelected(View v, int position);
+    }
+
+    private List<T> mList = new ArrayList<>();
+
+    public List<T> getmList() {
+        return mList;
+    }
+
+    public AppsAdapter(List<T> datas) {
+        mList.clear();
+        mList.addAll(datas);
+    }
+
+    OnItemClickListener onItemClickListener;
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    OnItemSelectedListener onItemSelectedListener;
+
+    public void setOnItemSelectedListener(OnItemSelectedListener onItemSelectedListener) {
+        this.onItemSelectedListener = onItemSelectedListener;
+    }
+
+    public void setData(List<T> data) {
+        mList.clear();
+        mList.addAll(data);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public AppsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_apps_recyclerview, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+
+//        holder.tv.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                onItemClickListener.onItemClick(v);
+//            }
+//        });
+//        holder.iv_icon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                onItemClickListener.onItemClick(v);
+//            }
+//        });
+        holder.layout_content.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onItemClickListener.onItemClick(v);
+            }
+        });
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final AppsAdapter.ViewHolder holder, final int position) {
+        String content = ((MyAppInfo) mList.get(position)).getAppName();
+        holder.tv_name.setText(content);
+        String content1 = ((MyAppInfo) mList.get(position)).getPackageName();
+        holder.tv_package.setText(content1);
+
+        Drawable icon = ((MyAppInfo) mList.get(position)).getImage();
+//        holder.tv.setTag(position);
+
+        holder.iv_icon.setImageDrawable(icon);
+
+        holder.layout_content.setTag(position);
+
+        /** 设置Item的Selected事件 */
+//        holder.tv.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//            @Override
+//            public void onFocusChange(View v, boolean hasFocus) {
+//                Log.e("RecyclerAdapter", "onFocusChange hasFocus:" + hasFocus + ",position:" + position);
+//                if (hasFocus) {
+//                    if (onItemSelectedListener != null) {
+//                        onItemSelectedListener.onItemSelected(holder.itemView, position);
+//                    }
+//                }
+//            }
+//        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        LinearLayout layout_content;
+        ImageView iv_icon;
+        TextView tv_name;
+        TextView tv_package;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            layout_content = itemView.findViewById(R.id.layout_content);
+            iv_icon = itemView.findViewById(R.id.iv_icon);
+            tv_name = itemView.findViewById(R.id.tv_name);
+            tv_package = itemView.findViewById(R.id.tv_package);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/italkbb/apklist/MainActivity.java
@@ -0,0 +1,146 @@
+package com.italkbb.apklist;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements AppsAdapter.OnItemClickListener {
+    @BindView(R.id.tv_title)
+    TextView tv_title;
+    @BindView(R.id.rv)
+    RecyclerView rv;
+
+    @BindView(R.id.progressbar)
+    ProgressBar progressbar;
+
+    AppsAdapter appsAdapter;
+    List<MyAppInfo> myAppInfoList;
+
+    Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        init();
+
+    }
+
+    protected void init() {
+
+//        LinearLayoutManager lm = new LinearLayoutManager(this);
+
+        GridLayoutManager glm = new GridLayoutManager(this, 3);
+        rv.setLayoutManager(glm);
+
+        myAppInfoList = new ArrayList<>();
+        appsAdapter = new AppsAdapter(myAppInfoList);
+        appsAdapter.setOnItemClickListener(this);
+
+        rv.setAdapter(appsAdapter);
+
+        Animation alphaAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.text_twinkle);
+
+        tv_title.startAnimation(alphaAnimation);
+
+        initData();
+    }
+
+    private void initData() {
+        progressbar.setVisibility(View.VISIBLE);
+
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                //扫描得到APP列表
+                final List<MyAppInfo> appInfos = ApkTool.scanLocalInstallAppList(MainActivity.this.getPackageManager());
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        appsAdapter.setData(appInfos);
+                        if (appInfos.size() > 0) {
+                            rv.requestFocus();
+                        }
+                        progressbar.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+        }.start();
+    }
+
+    @Override
+    public void onItemClick(View v) {
+        int position = (int) v.getTag();
+        startThridApp(this, ((MyAppInfo) appsAdapter.getmList().get(position)).getPackageName());
+    }
+
+    /**
+     * 通过包名启动第三方app
+     *
+     * @param context
+     * @param packageName
+     */
+    public static void startThridApp(Context context, String packageName) {
+        try {
+
+            Intent minIntent = context.getPackageManager()
+                    .getLaunchIntentForPackage(packageName);
+            context.startActivity(minIntent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_MENU:
+
+                ApkTool.customOnly = !ApkTool.customOnly;
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        //扫描得到APP列表
+                        final List<MyAppInfo> appInfos = ApkTool.scanLocalInstallAppList(MainActivity.this.getPackageManager());
+                        mHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                appsAdapter.setData(appInfos);
+                                if (appInfos.size() > 0) {
+                                    rv.requestFocus();
+                                }
+                                progressbar.setVisibility(View.INVISIBLE);
+                            }
+                        });
+                    }
+                }.start();
+
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+}
+
